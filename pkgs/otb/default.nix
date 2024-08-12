0fda60dbd3b348d4f230a5be0a94e09db1055369
daspk <dpratyush.k@gmail.com>
2024-08-12 15:24:16 +0700

    [ADD] Add support for multiple new OTB remote modules
    * Enabled optional integration of various OTB remote modules like MLUtils, Prefetch, and Phenology.

--- pkgs/otb/default.nix
+++ pkgs/otb/default.nix
@@ -9,6 +9,7 @@
   boost,
   curl,
   gdal,
+  gsl,
   itk_4_13,
   libsvm,
   libgeotiff,
@@ -30,6 +31,17 @@
   enableSAR ? true,
   enableSegmentation ? true,
   enableStereoProcessing ? true,
+  enablePrefetch ? true,
+  enableOtbtf ? true,
+  enableMLUtils ? false,
+  enablePhenology ? false,
+  enableBioVars ? false,
+  enableGRM ? false,
+  enableLSGRM ? false,
+  enableSimpleExtraction ? false,
+  enableTemporalGapfilling ? true,
+  enableTimeSeriesUtils ? false,
+  enableTemporalSmoothing ? false,
   pkgs,
   ...
 }: let
@@ -43,6 +55,86 @@
     ])
     ++ (extraPythonPackages python3.pkgs);
   otb-shark = shark.override {enableOpenMP = enableOpenMP;};
+
+  # remote modules based on :
+  # https://forgemia.inra.fr/orfeo-toolbox
+  # https://gitlab.orfeo-toolbox.org/orfeotoolbox/otb/-/tree/develop/Modules/Remote?ref_type=heads
+  mlUtils = builtins.fetchGit {
+    name = "otb-mlutils";
+    url = "https://forgemia.inra.fr/orfeo-toolbox/otb-mlutils.git";
+    ref = "master";
+    rev = "4f6de3654b249de98d5d5fef9c4bf4b623280ce4";
+  };
+
+  otbPrefetch = builtins.fetchGit {
+    name = "otb-prefetch";
+    url = "https://github.com/remicres/otb-prefetch.git";
+    ref = "main";
+    rev = "1faaa10d79e393bd45da95dde590f4857910c0ce";
+  };
+
+  otbTF = builtins.fetchGit {
+    name = "otbtf";
+    url = "https://forgemia.inra.fr/orfeo-toolbox/otbtf.git";
+    ref = "refs/tags/r4.3.1";
+    rev = "c9b02fb7d1ed5c28a45dc40dd15bfb4b59b77e95";
+  };
+
+  otbPhenology = builtins.fetchGit {
+    name = "otb-phenology";
+    url = "https://gitlab.orfeo-toolbox.org/jinglada/phenotb.git";
+    ref = "master";
+    rev = "85e991ab23695097ae59e034992695556fde5b2c";
+  };
+
+  otbBioVars = builtins.fetchGit {
+    name = "otb-biovars";
+    url = "https://gitlab.orfeo-toolbox.org/jinglada/otb-bv.git";
+    ref = "master";
+    rev = "519297118a6bbce819159e46432f83f4a5879d93";
+  };
+
+  otbGRM = builtins.fetchGit {
+    name = "otb-GRM";
+    url = "https://gitlab.irstea.fr/remi.cresson/GRM.git";
+    ref = "master";
+    rev = "0aca878d19c98d33fe7870ad9f37340235c6a0eb";
+  };
+
+  otbLSGRM = builtins.fetchGit {
+    name = "otb-LSGRM";
+    url = "https://gitlab.irstea.fr/remi.cresson/LSGRM.git";
+    ref = "disassembled";
+    rev = "52e1b0c0bd89eea2a94ac931a39da1b0e5a71833";
+  };
+
+  otbSeTools = builtins.fetchGit {
+    name = "otb-simpleextractiontools";
+    url = "https://forgemia.inra.fr/orfeo-toolbox/otb-simpleextractiontools.git";
+    ref = "master";
+    rev = "caad8a1b7a5858638c13e01866bbb45a9e2a87e5";
+  };
+
+  otbTempGapfill = builtins.fetchGit {
+    name = "otb-temporalgapfilling";
+    url = "https://gitlab.orfeo-toolbox.org/jinglada/temporalgapfilling.git";
+    ref = "master";
+    rev = "88e4e4254f17e51e908f622d364826da9c367a95";
+  };
+
+  otbTsUtils = builtins.fetchGit {
+    name = "otb-timeseriesutils";
+    url = "https://gitlab.irstea.fr/remi.cresson/TimeSeriesUtils.git";
+    ref = "master";
+    rev = "f0da48e07f9d09b2081cb6bdcbfcb4d189d38051";
+  };
+
+  otbTsSmooth = builtins.fetchGit {
+    name = "otb-temporalsmoothing";
+    url = "https://gitlab.irstea.fr/remi.cresson/TemporalSmoothing.git";
+    ref = "master";
+    rev = "15d0d710c3fe88a723e52c4ccc6c03fc7f669a0d";
+  };
 in
   stdenv.mkDerivation rec {
     pname = "otb";
@@ -55,6 +147,34 @@ in
       rev = versionMeta.rev;
     };
 
+    postPatch = lib.concatStringsSep "\n" (
+      (optionals enableMLUtils ["ln -sr ${mlUtils} Modules/Remote/MLUtils"])
+      ++ (optionals enablePrefetch ["ln -sr ${otbPrefetch} Modules/Remote/OTBPrefetch"])
+      ++ (optionals enableOtbtf ["ln -sr ${otbTF} Modules/Remote/otbtf"])
+      ++ (optionals enablePhenology [
+        "ln -sr ${otbPhenology} Modules/Remote/OTBPhenology"
+        "rm Modules/Remote/phenotb.remote.cmake"
+      ])
+      ++ (optionals enableBioVars [
+        "ln -sr ${otbBioVars} Modules/Remote/OTBBioVars"
+        "rm Modules/Remote/otb-bv.remote.cmake"
+      ])
+      ++ (optionals enableGRM [
+        "ln -sr ${otbGRM} Modules/Remote/OTBGRM"
+        "rm Modules/Remote/otbGRM.remote.cmake"
+      ])
+      ++ (optionals enableLSGRM [
+        "cp --no-preserve=mode -r ${otbLSGRM} Modules/Remote/OTBLSGRM"
+        "substituteInPlace Modules/Remote/OTBLSGRM/otb-module.cmake --replace \"OTBMPI\" \"\""
+      ])
+      ++ (optionals enableSimpleExtraction ["ln -sr ${otbSeTools} Modules/Remote/OTBSimpleExtractionTools"])
+      ++ (optionals enableTemporalGapfilling [
+        "ln -sr ${otbTempGapfill} Modules/Remote/OTBTemporalGapFilling"
+        "rm Modules/Remote/temporal-gapfilling.remote.cmake"
+      ])
+      ++ (optionals enableTimeSeriesUtils ["ln -sr ${otbTsUtils} Modules/Remote/OTBTimeSeriesUtils"])
+      ++ (optionals enableTemporalSmoothing ["ln -sr ${otbTsSmooth} Modules/Remote/OTBTemporalSmoothing"])
+    );
     nativeBuildInputs =
       [
         cmake
@@ -80,6 +200,10 @@ in
         perl
         otb-shark
         tinyxml
+      ]
+      ++ optionals enableTemporalGapfilling
+      [
+        gsl
       ]
       ++ optionals enablePython [
         python3
@@ -119,6 +243,40 @@ in
       ]
       ++ optionals doInstallCheck [
         "-DBUILD_TESTING=ON"
+      ]
+      ++ optionals enablePrefetch [
+        "-DModule_OTBPrefetch=ON"
+      ]
+      ++ optionals enableOtbtf [
+        "-DOTB_USE_TENSORFLOW=OFF"
+        "-DModule_OTBTensorflow=ON"
+      ]
+      ++ optionals enableMLUtils [
+        "-DModule_MLUtils=ON"
+      ]
+      ++ optionals enablePhenology [
+        "-DModule_OTBPhenology=ON"
+      ]
+      ++ optionals enableBioVars [
+        "-DModule_OTBBioVars=ON"
+      ]
+      ++ optionals enableGRM [
+        "-DModule_otbGRM=ON"
+      ]
+      ++ optionals enableLSGRM [
+        "-DModule_LSGRM=ON"
+      ]
+      ++ optionals enableSimpleExtraction [
+        "-DModule_SimpleExtractionTools=ON"
+      ]
+      ++ optionals enableTemporalGapfilling [
+        "-DModule_OTBTemporalGapFilling=ON"
+      ]
+      ++ optionals enableTimeSeriesUtils [
+        "-DModule_TimeSeriesUtils=ON"
+      ]
+      ++ optionals enableTemporalSmoothing [
+        "-DModule_TemporalSmoothing=ON"
       ];
 
     propagatedBuildInputs =
@@ -131,7 +289,7 @@ in
 
     # Make PATH available to subprocesses
     makeWrapperArgs = [
-        "--prefix PATH : ${computed_PATH}"
+      "--prefix PATH : ${computed_PATH}"
     ];
 
     #    pythonPath = optionals enablePython pythonInputs ++ ["$out/lib/otb/python"];

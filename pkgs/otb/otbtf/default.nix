7eb7fcc77d89db19c0d8c2493a802e0780065ad2
daspk <dpratyush.k@gmail.com>
2025-03-26 23:24:51 +0700

    [BUILD] Update OTBTF
    * 5.0.0rc4 -> 5.0.0

--- pkgs/otb/otbtf/default.nix
+++ pkgs/otb/otbtf/default.nix
@@ -1,12 +1,12 @@
 #  This module includes code from the OTB remote module otbtf
 #   Copyright 2018-2019 Rémi Cresson (IRSTEA)
-#   Copyright 2020-2021 Rémi Cresson (INRAE)
+#   Copyright 2020-2025 Rémi Cresson (INRAE)
 #  https://forgemia.inra.fr/orfeo-toolbox/otbtf/-/blob/develop/LICENSE
 {pkgs, fetchFromGitLab, ...}:
 fetchFromGitLab {
     owner = "orfeo-toolbox";
     repo = "otbtf";
-    rev = "5.0.0rc4";
-    hash = "sha256-OpiotQvFEYiYpY2ZCjF4zOnN+mTE4oof913BXBgfDAE=";
+    rev = "5.0.0";
+    hash = "sha256-XVE4b6gc9HBT49ztms0DbkKu22qrhGUK0tU+MU2jDao=";
     domain = "forgemia.inra.fr";
 }

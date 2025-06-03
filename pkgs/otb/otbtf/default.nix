d5e5a62081e07825adf9831ac9377f751f835040
Pratyush Das <28738918+daspk04@users.noreply.github.com>
2025-06-03 14:29:03 +0700

    Update NixOS-25.05 (#29)
    * [REFAC] Remove old ITK and Shark packages.
    
    * Remove the `itk_4_13_3` and `shark` packages as these have been added to upstream Nixpkgs
    
    * [REFAC] Update OTB build and dependencies in Nix flake
    
    * Update the OTB package and nixpkgs input and TensorFlow source hashes for compatibility with nixos-25.05.
    
    * [DOC] Update documentation for 25.05
    
    * [FIX] Remove duplicate definition
    
    * [FIX] Fix buildInputs in OTB package definition
    
    * Ensure `oldAttrs.buildInputs` defaults to an empty list if unset.
    
    * [FIX] Fix typo in fetchFromGitHub function call
    
    * [FIX] Fix broken modules for OTB
    
    * Comment out broken modules due to ITK 5.3.0 incompatibility.
    * Add ITK dependency to `default.nix`.
    
    * [FIX] Remove magic-nix-cache-action from CI workflow

--- pkgs/otb/otbtf/default.nix
+++ pkgs/otb/otbtf/default.nix
@@ -2,11 +2,10 @@
 #   Copyright 2018-2019 Rémi Cresson (IRSTEA)
 #   Copyright 2020-2025 Rémi Cresson (INRAE)
 #  https://forgemia.inra.fr/orfeo-toolbox/otbtf/-/blob/develop/LICENSE
-{pkgs, fetchFromGitLab, ...}:
-fetchFromGitLab {
-    owner = "orfeo-toolbox";
+{pkgs, fetchFromGitHub, ...}:
+fetchFromGitHub {
+    owner = "remicres";
     repo = "otbtf";
-    rev = "5.0.0";
-    hash = "sha256-6VqjuydvTmP+ES6xLQ8uSGTw/+ynYui+QkGXerYkZX8=";
-    domain = "forgemia.inra.fr";
+    tag = "5.0.0";
+    hash = "sha256-/UIQpaXJgQq3JOzibWHBlMqlBdp6lcNmMOv7hO0J2w4=";
 }
